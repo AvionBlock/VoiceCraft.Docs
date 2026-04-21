a8965f3ac58479b63027b6cf64bc9602d648d87a
lil-jon-crunk <git@playuw.com>
2026-04-21 02:34:08 +0200

    Add telemetry dashboard

---
+++ pages/telemetry.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import VoiceCraftTelemetryStats from '~/components/VoiceCraftTelemetryStats.vue'
+
+const { t } = useI18n()
+
+useSeoMeta({
+  title: () => String(t('telemetry.title')),
+  description: () => String(t('telemetry.subtitle')),
+})
+</script>
+
+<template>
+  <UContainer class="py-8 md:py-12">
+    <VoiceCraftTelemetryStats />
+  </UContainer>
+</template>
