fa583941abaa85b4e5162839b724480e09a085b9
lil-jon-crunk <git@playuw.com>
2026-05-13 05:15:10 +0300

    Set BS theme

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+useHead({
+  htmlAttrs: {
+    class: 'dark',
+    'data-bs-theme': 'dark',
+  },
+  bodyAttrs: {
+    'data-bs-theme': 'dark',
+  },
+})
+</script>
+
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLayout>
